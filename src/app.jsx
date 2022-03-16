aefda64c3048af04b762aa2b1806bfafcf8df127
Haresh <haresh2569@gmail.com>
2022-03-16 07:20:52 +0530

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "./header";
+import Note from "./note";
+import Footer from "./footer";
+function App(){
+    return <span> <Footer/> <Note/> <Header/></span>
+}
+
+export default App
---
+++ src/footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Footer(){
+    const year =new Date().getFullYear()
+    return <footer><p>Copyrights@ {year}</p></footer>
+}
+
+export default Footer
